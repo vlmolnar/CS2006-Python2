cdcde461eb2c990672a0613d5eb0393a7e471a01
vlmolnar <lora9@hotmail.com>
2018-03-28 12:50:50 +0100

    Created basic files

---
+++ code/analysis.py
@@ -0,0 +1,4 @@
+# – calculate the total number of tweets, retweets and replies in the dataset
+# – calculate the number of different users tweeting in this dataset
+# – calculate the average number of tweets, retweets and replies sent by a user
+# – identify most popular hashtags
---
+++ code/visualisation.py
@@ -0,0 +1,5 @@
+# build plots/visualisations for:
+# – the structure of the dataset (tweets/retweets/replies)
+# – the timeline of the tweets activity
+# – the word cloud for all other hashtags used in the tweets from the dataset
+import matplotlib as mpl
