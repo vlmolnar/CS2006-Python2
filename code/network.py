67c330906aa2762985690e545c4daef4a4513efb
Jonathan <jonathan.short@live.co.uk>
2018-04-26 23:59:26 +0100

    started network

--- code/network.py
+++ code/network.py
@@ -8,26 +8,51 @@ import re
 def getNodesOfUsers(df):
     return list(df["from_user"].unique())
 
-def get_edges_of_users(df):
+def getEdgesOfRetweets(df):
     count = 0
-    edges = []
+    retweet_edges = []
+    mentions_edges = []
+    replies_edge = []
     for i,row in df.iterrows():
-        match = re.search("RT @", row["text"])
-        if match != None:
-            words = row["text"].split(" ")
-            user = words[1][1:]
-            from_user = row["from_user"]
-            print("from", from_user, "user:", user)
-            edges.append((user, from_user))
-        if count == 10: break
-        count += 1
+        from_user = row["from_user"]
+        # if reply
+        if pd.notnull(row["in_reply_to_screen_name"]):
+            replies_edge.append((from_user, row["in_reply_to_screen_name"]))
+
+        words = row["text"].split(" ")
+        for word in words:
+            match = re.search("@", word)
+            if match != None:
+                index = words.index(word)
+                user = words[index][1:]
+                if words[index - 1] == "RT":     # if retweet
+                    retweet_edges.append((from_user, user))
+                else:
+                    mentions_edges.append((from_user, user))
+    return retweet_edges, mentions_edges, replies_edge
+
+def drawNetwork(df):
+    nodes = getNodesOfUsers(df)
+    retweet_edges, mentions_edges, replies_edge = getEdgesOfRetweets(df)
+    G = nx.Graph()
+    G.add_nodes_from(nodes)
+    G.add_edges_from(retweet_edges)
+    G.add_edges_from(mentions_edges)
+    G.add_edges_from(replies_edge)
+    for u,v in retweet_edges:
+        G[u][v]['color'] = "red"
+    # [G[u][v]['color'] = "blue" for u,v in mentions_edges]
+    # [G[u][v]['color'] = "green" for u,v in replies_edge]
+
+    nx.draw(G)
+    plt.show()
     return
 
 def drawSquare(G):
     corners = [1,2,3,4]
     edges = [(1,2), (2,3), (3,4), (4,1)]
     G.add_nodes_from(corners)
-    G.add_edges_from(edges)
+    G.add_edges_from(edges, color='b')
 
     nx.draw(G, with_labels=True)
     plt.show()
