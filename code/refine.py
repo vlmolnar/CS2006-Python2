ddc0cc7581622447da60f36da90fa12d7e4f7c82
vlmolnar <vlm5@pc2-101-l.cs.st-andrews.ac.uk>
2018-04-27 14:44:29 +0100

    Added documentation

--- code/refine.py
+++ code/refine.py
@@ -1,9 +1,3 @@
-# refine the dataset:
-# - develop a procedure to check that the data match expected format and remove duplicates
-# – in case of any inconsistencies and/or duplicates found, produce new file with refined data
-#   to be used in the subsequent analysis
-# – this step must be automated and documented in case one may need to re-run it, although it’s
-#   not necessary to repeat it each time while re-running the analysis
 import pandas as pd
 import sys
 import re
